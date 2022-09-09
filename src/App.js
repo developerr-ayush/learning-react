d0cf4212f86bb8cb3a1c65e849d2fe773edc5681
shahayush480 <shahayush480@gmail.com>
2022-09-09 09:24:57 +0530

    learned React Router

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import './App.css';
 import About from './components/About';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
+
 
 function App() {
   return (
     <>
-      <Navbar title="My Website"/>
-
-      {/* <Textform heading="Form convert texts"/> */}
-      <About/>
+      <Navbar title="My Website" />
+      <Routes>
+        <Route path='/' element={<Textform heading="Form convert texts" />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
     </>
   );
 }
